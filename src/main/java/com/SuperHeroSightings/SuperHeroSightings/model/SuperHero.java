3d10b15eedbc5ecd1964ae011251e975a2b8ed25
yareenm <yareenm@outlook.com>
2022-11-22 21:39:11 -0600

    SuperHero class functions are added

--- src/main/java/com/SuperHeroSightings/SuperHeroSightings/model/SuperHero.java
+++ src/main/java/com/SuperHeroSightings/SuperHeroSightings/model/SuperHero.java
@@ -1,5 +1,7 @@
 package com.SuperHeroSightings.SuperHeroSightings.model;
 
+import java.util.Objects;
+
 public class SuperHero {
 
     private int superID;
@@ -9,5 +11,51 @@ public class SuperHero {
     private String superDescription;
 
     private String superPower;
+
+    public int getSuperID() {
+        return superID;
+    }
+
+    public void setSuperID(int superID) {
+        this.superID = superID;
+    }
+
+    public String getSuperName() {
+        return superName;
+    }
+
+    public void setSuperName(String superName) {
+        this.superName = superName;
+    }
+
+    public String getSuperDescription() {
+        return superDescription;
+    }
+
+    public void setSuperDescription(String superDescription) {
+        this.superDescription = superDescription;
+    }
+
+    public String getSuperPower() {
+        return superPower;
+    }
+
+    public void setSuperPower(String superPower) {
+        this.superPower = superPower;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SuperHero superHero = (SuperHero) o;
+        return superID == superHero.superID && Objects.equals(superName, superHero.superName) && Objects.equals(superDescription, superHero.superDescription) && Objects.equals(superPower, superHero.superPower);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(superID, superName, superDescription, superPower);
+    }
+
 
 }
