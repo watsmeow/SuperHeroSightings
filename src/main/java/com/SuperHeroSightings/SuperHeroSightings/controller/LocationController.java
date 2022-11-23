ddf6bace3599a1c9d46dcc9685fbaf2018f44b09
Courtney Bowe <csmyers89@gmail.com>
2022-11-23 10:36:25 -0600

    Started on LocationController (added dao declaration and constructor)

--- src/main/java/com/SuperHeroSightings/SuperHeroSightings/controller/LocationController.java
+++ src/main/java/com/SuperHeroSightings/SuperHeroSightings/controller/LocationController.java
@@ -1,4 +1,14 @@
 package com.SuperHeroSightings.SuperHeroSightings.controller;
 
+import com.SuperHeroSightings.SuperHeroSightings.dao.LocationDao;
+
 public class LocationController {
+    // Declare Dao objects
+    private final LocationDao locationDao;
+
+    public LocationController(LocationDao locationDao) {
+        this.locationDao = locationDao;
+    }
+
+
 }
