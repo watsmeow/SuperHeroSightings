3e2c02edc2b4d26db84a657ae26a5978ecf79db3
sarahthuang <sth2127@barnard.edu>
2022-11-29 00:36:53 -0500

    In-process sighting controller + sightings.html

--- src/main/java/com/SuperHeroSightings/SuperHeroSightings/controller/SightingController.java
+++ src/main/java/com/SuperHeroSightings/SuperHeroSightings/controller/SightingController.java
@@ -1,7 +1,50 @@
 package com.SuperHeroSightings.SuperHeroSightings.controller;
 
+import com.SuperHeroSightings.SuperHeroSightings.dao.LocationDao;
+import com.SuperHeroSightings.SuperHeroSightings.dao.OrganizationDao;
+import com.SuperHeroSightings.SuperHeroSightings.dao.SightingDao;
+import com.SuperHeroSightings.SuperHeroSightings.dao.SuperHeroesDao;
+import com.SuperHeroSightings.SuperHeroSightings.model.Sighting;
+import com.SuperHeroSightings.SuperHeroSightings.model.SuperHero;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.util.List;
 
 @Controller
 public class SightingController {
+
+    @Autowired
+    SightingDao sightingDao;
+
+    @Autowired
+    LocationDao locationDao;
+
+    @Autowired
+    OrganizationDao organizationDao;
+
+    @Autowired
+    SuperHeroesDao superHeroesDao;
+
+    @GetMapping("sightings")
+    public String displaySightings(Model model){
+        List<Sighting> sightings = sightingDao.getAllSightings();
+        model.addAttribute("sightings",sightings);
+        return "sightings";
+    }
+
+    @PostMapping("addSighting")
+    public String addSighting(HttpServletRequest request) {
+        String timestamp = request.getParameter("timestamp");
+        Timestamp newTimeStamp = Timestamp.valueOf(timestamp);
+
+        //TODO
+
+        return "redirect:/sightings";
+    }
 }
