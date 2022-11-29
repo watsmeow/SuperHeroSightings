59ba9587d76903916fe0c83a8a75ce3b8314bce0
Andrea Watson <awatson.e@gmail.com>
2022-11-29 13:36:23 -0500

    added newsfeed

--- src/main/java/com/SuperHeroSightings/SuperHeroSightings/controller/SightingController.java
+++ src/main/java/com/SuperHeroSightings/SuperHeroSightings/controller/SightingController.java
@@ -1,7 +1,24 @@
 package com.SuperHeroSightings.SuperHeroSightings.controller;
 
+import com.SuperHeroSightings.SuperHeroSightings.dao.SightingDao;
+import com.SuperHeroSightings.SuperHeroSightings.model.Sighting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class SightingController {
+
+    @Autowired
+    SightingDao sightingDao;
+
+    @GetMapping("/")
+    public String newsFeedSightings(Model model) {
+        List<Sighting> newsFeedSightings = sightingDao.newsFeedSightings();
+        model.addAttribute("newsFeedSightings", newsFeedSightings);
+        return "index";
+    }
 }
