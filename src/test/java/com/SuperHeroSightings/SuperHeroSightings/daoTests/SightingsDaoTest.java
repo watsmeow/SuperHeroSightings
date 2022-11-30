0826c89fae334db8afff76a69d9513dbea4a0165
sarahthuang <sth2127@barnard.edu>
2022-11-30 14:18:18 -0500

    small change in sightingdaotest.java

--- src/test/java/com/SuperHeroSightings/SuperHeroSightings/daoTests/SightingsDaoTest.java
+++ src/test/java/com/SuperHeroSightings/SuperHeroSightings/daoTests/SightingsDaoTest.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
