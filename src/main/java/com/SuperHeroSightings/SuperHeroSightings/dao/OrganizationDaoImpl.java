50a8ea6ea8334745362d30eda2a48d5421618ff1
Courtney Bowe <csmyers89@gmail.com>
2022-12-01 00:10:50 -0600

    Cleaning up

--- src/main/java/com/SuperHeroSightings/SuperHeroSightings/dao/OrganizationDaoImpl.java
+++ src/main/java/com/SuperHeroSightings/SuperHeroSightings/dao/OrganizationDaoImpl.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
